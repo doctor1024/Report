f0e13cb8857786e39cce01b1cadea2a7a41d94ad
Jinhyung Park <Jin@Jinhyungs-MacBook-Air.local>
2017-02-19 23:10:50 +0900

    Add mini-02-create class.py - Make a classes

---
+++ mini-02-create class.py
@@ -0,0 +1,10 @@
+# 학교 클래스 만들기 / 이름,설립연도,주소 정보 필요
+
+class School:
+    name = "이름"
+    foundation = "설림연도"
+    address = "주소"
+
+    def __init__(self, foundation, address)
+        self.foundation = foundation
+        self.address = address
